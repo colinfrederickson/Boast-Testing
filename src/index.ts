35825a1821b0d33f41805887199c1628f3100c48
colinfrederickson <110066636+colinfrederickson@users.noreply.github.com>
2023-11-29 11:42:11 -0600

    Refactor snapshot creation and enhance error handling in Flatfile integration
    This commit introduces several key improvements to the Flatfile integration for snapshot creation functionality:
    
    1. **Modularization of Snapshot Creation**:
       - Extracted the snapshot creation logic into a separate `createSnapshot` function. This function makes the API call to create a snapshot and can be easily reused and tested.
    
    2. **Improved Input Validation**:
       - Added validation checks for `sheetId` and `jobId` within the main event handler. This ensures that the function only proceeds if the inputs are valid, reducing the risk of runtime errors.
    
    3. **Enhanced Error Handling**:
       - Expanded the error handling mechanism to provide more detailed feedback. The code now differentiates between API-specific errors, request-related errors, and general JavaScript errors. This makes debugging and troubleshooting more efficient.
    
    These changes aim to enhance the code's maintainability, readability, and overall reliability, particularly in handling API interactions and input data.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { xlsxExtractorPlugin } from '@flatfile/plugin-xlsx-extractor'
 import { configureSpace } from './jobs/space/configure'
 import { validateRecords } from './jobs/sheet/validateRecords'
 import mergeSelectedRecords from './jobs/sheet/mergeSelectedRecords'
+import createSnapshot from './jobs/sheet/createSnapshot'
 
 /**
  * This default export is used by Flatfile to register event handlers for any
@@ -14,5 +15,6 @@ export default function (listener: FlatfileListener) {
   listener.use(configureSpace)
   listener.use(validateRecords)
   listener.use(mergeSelectedRecords)
+  listener.use(createSnapshot)
   listener.use(xlsxExtractorPlugin({ rawNumbers: true }))
 }
---
+++ src/jobs/sheet/createSnapshot.ts
@@ -0,0 +1,75 @@
+import { FlatfileListener } from '@flatfile/listener'
+import api from '@flatfile/api'
+
+// Separate function to handle the snapshot creation
+async function createSnapshot(sheetId) {
+  return await api.snapshots.createSnapshot({ sheetId })
+}
+
+export default function (listener: FlatfileListener) {
+  listener.filter({ job: 'sheet:createSnapshot' }, (configure) => {
+    configure.on('job:ready', async (event) => {
+      const { jobId, sheetId } = event.context
+
+      // Input Validation
+      if (typeof sheetId !== 'string' || sheetId.trim() === '') {
+        console.error('Invalid sheetId provided')
+        await api.jobs.fail(jobId, {
+          info: 'Invalid sheetId provided for snapshot creation.',
+        })
+        return // Exit the function if sheetId is not valid
+      }
+
+      if (typeof jobId !== 'string' || jobId.trim() === '') {
+        console.error('Invalid jobId provided')
+        return // Exit the function early if jobId is not valid
+      }
+
+      try {
+        await api.jobs.ack(jobId, {
+          info: 'Preparing to Create Snapshot...',
+          progress: 10,
+        })
+
+        console.log(`Creating snapshot for sheetId: ${sheetId}`)
+
+        // Call the createSnapshot function
+        const response = await createSnapshot(sheetId)
+
+        if (response && response.data && response.data.id) {
+          console.log(`Snapshot created successfully: ${response.data.id}`)
+
+          await api.jobs.complete(jobId, {
+            info: 'Snapshot created successfully.',
+            outcome: {
+              acknowledge: true,
+              message: `Snapshot of sheet ${sheetId} created successfully.`,
+            },
+          })
+        } else {
+          throw new Error('Snapshot creation failed')
+        }
+      } catch (error) {
+        // More detailed error handling
+        if (error.response) {
+          // API-specific errors
+          console.error(
+            `API Error: ${error.response.status} - ${JSON.stringify(
+              error.response.data
+            )}`
+          )
+        } else if (error.request) {
+          // Errors related to the request made but no response received
+          console.error(`Request Error: ${error.request}`)
+        } else {
+          // General errors
+          console.error(`Error: ${error.message}`)
+        }
+
+        await api.jobs.fail(jobId, {
+          info: 'Error occurred during the snapshot creation.',
+        })
+      }
+    })
+  })
+}
